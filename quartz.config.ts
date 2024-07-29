c5b6eb366e71f8bee786a75b393c0065a5eef87d
Brennan Bibic <brennan.bibic@gmail.com>
2024-07-28 22:56:33 -0400

    Revert "Merge branch 'v4' of https://github.com/jackyzha0/quartz into v4"
    This reverts commit e6bde3299cc30f7f95b36e10a773bf9673dfe50f, reversing
    changes made to 889bb324784f0d2239a9ba153ec83dcbae2663c6.

--- quartz.config.ts
+++ quartz.config.ts
@@ -55,6 +55,7 @@ const config: QuartzConfig = {
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "filesystem"],
       }),
+      Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting({
         theme: {
           light: "github-light",
@@ -67,7 +68,6 @@ const config: QuartzConfig = {
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
-      Plugin.Latex({ renderEngine: "katex" }),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
